aa1e43c620c649e3ff70ba74a5c5ac2387311976
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-08-17 14:40:05 +0530

    user and register-> config, routes, and controller is done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config()
+const userAuthRoute = require("./routes/userAuth.js")
+//user route config
+app.use("/user", userAuthRoute)
 
 
 app.listen(process.env.PORT, () => {

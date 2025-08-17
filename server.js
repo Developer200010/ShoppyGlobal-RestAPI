82835e21346a472b4f8675bba388a2e8984cdb9f
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-08-17 14:46:26 +0530

    CRUD operation of Prouducts is done

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config()
-const userAuthRoute = require("./routes/userAuth.js")
+const userAuthRoute = require("./routes/userAuth.js");
+const productRoute = require("./routes/productsRoute.js");
 //user route config
 app.use("/user", userAuthRoute)
+
+// product route is config
+app.use("/products", productRoute);
 
 
 app.listen(process.env.PORT, () => {
